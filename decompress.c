2892fb8b2ddb038773eaf50025209333735b3e1d
Cong <congusbongus@gmail.com>
2024-03-09 19:16:18 +1100

    Visual studio

--- decompress.c
+++ decompress.c
@@ -6,87 +6,88 @@
 #ifdef _MSC_VER
 #include <io.h>
 #define close _close
+#define mkstemp _mktemp
 #else
 #include <unistd.h>
 #endif
 
-char *decompress(const char *ipath, size_t *len) {
-  FILE *ifile = NULL;
-  FILE *ofile = NULL;
-  char *buf = NULL;
-  int ver;
+char* decompress(const char* ipath, size_t* len) {
+	FILE* ifile = NULL;
+	FILE* ofile = NULL;
+	char* buf = NULL;
+	int ver;
 
-  if ((ifile = fopen(ipath, "rb")) == NULL) {
-    printf("'%s' :not found\n", ipath);
-    goto bail;
-  }
+	if ((ifile = fopen(ipath, "rb")) == NULL) {
+		printf("'%s' :not found\n", ipath);
+		goto bail;
+	}
 
-  if (rdhead(ifile, &ver) != SUCCESS) {
-    printf("'%s' is not LZEXE file.\n", ipath);
-    goto bail;
-  }
-  printf("file '%s' is compressed by LZEXE Ver. 0.%d\n", ipath, ver);
+	if (rdhead(ifile, &ver) != SUCCESS) {
+		printf("'%s' is not LZEXE file.\n", ipath);
+		goto bail;
+	}
+	printf("file '%s' is compressed by LZEXE Ver. 0.%d\n", ipath, ver);
 
-  char opath[FILENAME_MAX];
-  strcpy(opath, "decompressXXXXXX");
-  int fd = mkstemp(opath);
-  if (fd == -1) {
-    printf("can't create temp file.\n");
-    goto bail;
-  }
-  // Reopen in binary mode to be able to fseek
-  close(fd);
-  ofile = fopen(opath, "w+b");
-  if (ofile == NULL) {
-    printf("can't open '%s'.\n", opath);
-    goto bail;
-  }
+	char opath[FILENAME_MAX];
+	strcpy(opath, "decompressXXXXXX");
+	int fd = mkstemp(opath);
+	if (fd == -1) {
+		printf("can't create temp file.\n");
+		goto bail;
+	}
+	// Reopen in binary mode to be able to fseek
+	close(fd);
+	ofile = fopen(opath, "w+b");
+	if (ofile == NULL) {
+		printf("can't open '%s'.\n", opath);
+		goto bail;
+	}
 
-  if (mkreltbl(ifile, ofile, ver) != SUCCESS) {
-    goto bail;
-  }
-  if (unpack(ifile, ofile) != SUCCESS) {
-    goto bail;
-  }
-  fclose(ifile);
-  wrhead(ofile);
+	if (mkreltbl(ifile, ofile, ver) != SUCCESS) {
+		goto bail;
+	}
+	if (unpack(ifile, ofile) != SUCCESS) {
+		goto bail;
+	}
+	fclose(ifile);
+	wrhead(ofile);
 
-  // Read decompressed file into memory
-  if (fseek(ofile, 0, SEEK_END) != 0) {
-    printf("can't seek.\n");
-    goto bail;
-  }
-  *len = ftell(ofile);
-  if (fseek(ofile, 0, SEEK_SET) != 0) {
-    printf("can't seek.\n");
-    goto bail;
-  }
-  buf = malloc(*len);
-  if (buf == NULL) {
-    printf("can't allocate %d bytes\n", (int)*len);
-    goto bail;
-  }
-  if (fread(buf, 1, *len, ofile) != *len) {
-    if (feof(ofile))
-      printf("Error reading output file: unexpected end of file\n");
-    else if (ferror(ofile))
-      perror("Error reading output file");
-    goto bail;
-  }
-  printf("Read %d bytes\n", (int)*len);
+	// Read decompressed file into memory
+	if (fseek(ofile, 0, SEEK_END) != 0) {
+		printf("can't seek.\n");
+		goto bail;
+	}
+	*len = ftell(ofile);
+	if (fseek(ofile, 0, SEEK_SET) != 0) {
+		printf("can't seek.\n");
+		goto bail;
+	}
+	buf = malloc(*len);
+	if (buf == NULL) {
+		printf("can't allocate %d bytes\n", (int)*len);
+		goto bail;
+	}
+	if (fread(buf, 1, *len, ofile) != *len) {
+		if (feof(ofile))
+			printf("Error reading output file: unexpected end of file\n");
+		else if (ferror(ofile))
+			perror("Error reading output file");
+		goto bail;
+	}
+	printf("Read %d bytes\n", (int)*len);
 
-  fclose(ofile);
+	fclose(ofile);
 
-  return buf;
+	return buf;
 
 bail:
-  if (ifile) {
-    fclose(ifile);
-  }
-  if (ofile) {
-    fclose(ofile);
-  }
-  free(buf);
-  *len = 0;
-  return NULL;
+	if (ifile) {
+		fclose(ifile);
+	}
+	if (ofile) {
+		fclose(ofile);
+	}
+	free(buf);
+	*len = 0;
+	return NULL;
 }
