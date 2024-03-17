881802e666ca2b66f75d50667211c85cc6cd20d4
Cong <congusbongus@gmail.com>
2024-03-17 12:06:05 +1100

    Update main.c

--- main.c
+++ main.c
@@ -13,28 +13,32 @@
 #define stricmp strcasecmp /* v0.9 */
 char *backup_ext = ".olz";
 
-int fnamechg(char *ipath, char *opath, int rename_sw);
+int fnamechg(const char *ipath, char *opath, int rename_sw);
 void parsepath(char *pathname, int *fname, int *ext);
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   char ofname[FILENAME_MAX];
   FILE *ifile, *ofile;
   int ver, rename_sw = 0;
 
   printf("UNLZEXE Ver. " VERSION "\n");
-  if (argc != 3 && argc != 2) {
+  if (argc != 3 && argc != 2)
+  {
     printf("usage: UNLZEXE packedfile [unpackedfile]\n");
     exit(EXIT_FAILURE);
   }
   const char *ipath = argv[1];
   if (argc == 2)
     rename_sw = 1;
-  if ((ifile = fopen(ipath, "rb")) == NULL) {
+  if ((ifile = fopen(ipath, "rb")) == NULL)
+  {
     printf("'%s' :not found\n", ipath);
     exit(EXIT_FAILURE);
   }
 
-  if (rdhead(ifile, &ver) != SUCCESS) {
+  if (rdhead(ifile, &ver) != SUCCESS)
+  {
     printf("'%s' is not LZEXE file.\n", ipath);
     fclose(ifile);
     exit(EXIT_FAILURE);
@@ -42,33 +46,40 @@ int main(int argc, char **argv) {
   printf("file '%s' is compressed by LZEXE Ver. 0.%d\n", ipath, ver);
 
   char opath[FILENAME_MAX];
-  if (argc == 2) {
+  if (argc == 2)
+  {
     strcpy(opath, "unlzexeXXXXXX");
     int fd = mkstemp(opath);
-    if (fd == -1) {
+    if (fd == -1)
+    {
       printf("can't create temp file.\n");
       fclose(ifile);
       exit(EXIT_FAILURE);
     }
     // Reopen in binary mode to be able to fseek
     close(fd);
-  } else {
+  }
+  else
+  {
     strcpy(opath, argv[2]);
   }
   ofile = fopen(opath, "w+b");
-  if (ofile == NULL) {
+  if (ofile == NULL)
+  {
     printf("can't open '%s'.\n", opath);
     fclose(ifile);
     exit(EXIT_FAILURE);
   }
 
-  if (mkreltbl(ifile, ofile, ver) != SUCCESS) {
+  if (mkreltbl(ifile, ofile, ver) != SUCCESS)
+  {
     fclose(ifile);
     fclose(ofile);
     remove(opath);
     exit(EXIT_FAILURE);
   }
-  if (unpack(ifile, ofile) != SUCCESS) {
+  if (unpack(ifile, ofile) != SUCCESS)
+  {
     fclose(ifile);
     fclose(ofile);
     remove(opath);
@@ -78,22 +89,26 @@ int main(int argc, char **argv) {
   wrhead(ofile);
   fclose(ofile);
 
-  if (fnamechg(ipath, opath, rename_sw) != SUCCESS) {
+  if (fnamechg(ipath, opath, rename_sw) != SUCCESS)
+  {
     exit(EXIT_FAILURE);
   }
   exit(EXIT_SUCCESS);
 }
 
-int fnamechg(char *ipath, char *opath, int rename_sw) {
+int fnamechg(const char *ipath, char *opath, int rename_sw)
+{
   int idx_name, idx_ext;
   char tpath[FILENAME_MAX];
 
-  if (rename_sw) {
+  if (rename_sw)
+  {
     strcpy(tpath, ipath);
     parsepath(tpath, &idx_name, &idx_ext);
     strcpy(tpath + idx_ext, backup_ext);
     remove(tpath);
-    if (rename(ipath, tpath)) {
+    if (rename(ipath, tpath))
+    {
       printf("can't make '%s'.\n", tpath);
       remove(opath);
       return (FAILURE);
@@ -101,8 +116,10 @@ int fnamechg(char *ipath, char *opath, int rename_sw) {
     printf("'%s' is renamed to '%s'.\n", ipath, tpath);
   }
   strcpy(tpath, ipath);
-  if (rename(opath, tpath)) {
-    if (rename_sw) {
+  if (rename(opath, tpath))
+  {
+    if (rename_sw)
+    {
       strcpy(tpath, ipath);
       parsepath(tpath, &idx_name, &idx_ext);
       strcpy(tpath + idx_ext, backup_ext);
@@ -116,14 +133,17 @@ int fnamechg(char *ipath, char *opath, int rename_sw) {
   return (SUCCESS);
 }
 
-void parsepath(char *pathname, int *fname, int *ext) {
+void parsepath(char *pathname, int *fname, int *ext)
+{
   char c;
   int i;
 
   *fname = 0;
   *ext = 0;
-  for (i = 0; (c = pathname[i]); i++) {
-    switch (c) {
+  for (i = 0; (c = pathname[i]); i++)
+  {
+    switch (c)
+    {
     case ':':
     case '\\':
       *fname = i + 1;
